be368d263b6785a375c4e985e2be69318c6a2b62
gilnakar <g.nakar@gmail.com>
2015-07-31 23:07:51 +0300

    minor links and QTY fixes

--- js/controllers.js
+++ js/controllers.js
@@ -7,11 +7,3 @@ myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
 
 }]);
 
-// function multiply(){
-// var a=Number(document.
-//          getElementById('QTY').value);
-// var b=33;
-// var c=a*b;
-// document.getElementById('TOTAL').value=c;
-
-// };
